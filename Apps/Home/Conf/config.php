b1f607b06391bb8694c3cde9c8f37ca21ff5ddf3
lazyrong <lazytech@foxmail.com>
2016-05-05 01:38:19 +0800

    create sql

--- Apps/Home/Conf/config.php
+++ Apps/Home/Conf/config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
 
-	'default_module'     => 'Home', //默认模块
+	'Default_module'     => 'Home', //默认模块
 
 	'URL_MODEL'            => 2, //URL模式
 	'URL_CASE_INSENSITIVE'  =>  true,
@@ -23,6 +23,17 @@ return array(
 		    '__IMAGES__'	=>	__ROOT__.MODULE_PATH.'view/Public/images',
 
 	   	),
-	'SHOW_PAGE_TRACE' =>true
+	'SHOW_PAGE_TRACE' =>true,
+	
+	//数据库配置信息
+	'DB_TYPE'   => 'mysql', // 数据库类型
+	'DB_HOST'   => 'localhost', // 服务器地址
+	'DB_NAME'   => 'lazycms', // 数据库名
+	'DB_USER'   => 'root', // 用户名
+	'DB_PWD'    => '123456', // 密码
+	'DB_PORT'   => 3306, // 端口
+	'DB_PREFIX' => 'lazy_', // 数据库表前缀 
+	'DB_CHARSET'=> 'utf8' // 字符集
+
 
 );
--- Apps/Home/Controller/ListController.class.php
+++ Apps/Home/Controller/ListController.class.php
@@ -8,6 +8,11 @@ use Think\Controller;
 class ListController extends Controller{
 	
 	Public function index() {
+
+		$newsList=D('news')->select();
+		$this->assign('newsList',$newsList);
+		var_dump($newsList);
+		die;
 		$this->display('xwrw');
 	}
 	
